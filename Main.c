a423d4e3c49a8eff38e815f4980117778e54b97e
JoakimIngnas <joakim.ingnas.5744@student.uu.se>
2015-09-08 14:37:41 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,12 +1,1 @@
-#Include <stdio.h>
 
-char action;
-int n = 1;
-
-int main(){
-  while (n == 1){
-  printf("========LagerHanterare========\nChoose what action to perform;\n(A)dd an item.\n(R)emove an item.\n(E)dit an item.\n(L)ist all the items currently in the warehouse.\n(S)earch for an item in the warehouse\n(U)ndo the last change.\nE(x)it the program.\n");
-  scanf("%d", &action);}
-		 
-	      
-}
